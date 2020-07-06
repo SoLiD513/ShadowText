2a4f1d3c7480064e46a5e4508b7ba36730019ce4
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-05 20:32:21 -0400

    yay

---
+++ src/ShadowText/ShadowText.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./ShadowText.css";
+
+class ShadowText extends Component {
+  render() {
+    return (
+      <>
+        <div className="wrapper">
+          <div className="text-3d">Edit src/App.js and save to reload.</div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ShadowText;
